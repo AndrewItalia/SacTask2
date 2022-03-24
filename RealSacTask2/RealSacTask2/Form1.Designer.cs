78df56a4464186d3c10f71f92c2aab0c48f4a807
AndrewItalia <102033396+AndrewItalia@users.noreply.github.com>
2022-03-24 12:30:40 +1100

    Add files via upload

---
+++ RealSacTask2/RealSacTask2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace RealSacTask2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImportfile = new System.Windows.Forms.Button();
+            this.dgvBookSales = new System.Windows.Forms.DataGridView();
+            this.Textbook = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Seller = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Purchaser = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Purchaseprice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Saleprice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Profit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookSales)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnImportfile
+            // 
+            this.btnImportfile.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnImportfile.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImportfile.ForeColor = System.Drawing.Color.White;
+            this.btnImportfile.Location = new System.Drawing.Point(12, 27);
+            this.btnImportfile.Name = "btnImportfile";
+            this.btnImportfile.Size = new System.Drawing.Size(163, 62);
+            this.btnImportfile.TabIndex = 0;
+            this.btnImportfile.Text = "Calculate Profit";
+            this.btnImportfile.UseVisualStyleBackColor = false;
+            this.btnImportfile.Click += new System.EventHandler(this.btnImportfile_Click);
+            // 
+            // dgvBookSales
+            // 
+            this.dgvBookSales.AllowUserToAddRows = false;
+            this.dgvBookSales.AllowUserToDeleteRows = false;
+            this.dgvBookSales.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvBookSales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBookSales.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Textbook,
+            this.Subject,
+            this.Seller,
+            this.Purchaser,
+            this.Purchaseprice,
+            this.Saleprice,
+            this.Profit});
+            this.dgvBookSales.Location = new System.Drawing.Point(12, 109);
+            this.dgvBookSales.Name = "dgvBookSales";
+            this.dgvBookSales.ReadOnly = true;
+            this.dgvBookSales.RowHeadersWidth = 62;
+            this.dgvBookSales.RowTemplate.Height = 28;
+            this.dgvBookSales.Size = new System.Drawing.Size(1091, 349);
+            this.dgvBookSales.TabIndex = 1;
+            // 
+            // Textbook
+            // 
+            this.Textbook.HeaderText = "Textbook";
+            this.Textbook.MinimumWidth = 8;
+            this.Textbook.Name = "Textbook";
+            this.Textbook.ReadOnly = true;
+            this.Textbook.Width = 150;
+            // 
+            // Subject
+            // 
+            this.Subject.HeaderText = "Subject";
+            this.Subject.MinimumWidth = 8;
+            this.Subject.Name = "Subject";
+            this.Subject.ReadOnly = true;
+            this.Subject.Width = 150;
+            // 
+            // Seller
+            // 
+            this.Seller.HeaderText = "Seller";
+            this.Seller.MinimumWidth = 8;
+            this.Seller.Name = "Seller";
+            this.Seller.ReadOnly = true;
+            this.Seller.Width = 150;
+            // 
+            // Purchaser
+            // 
+            this.Purchaser.HeaderText = "Purchaser";
+            this.Purchaser.MinimumWidth = 8;
+            this.Purchaser.Name = "Purchaser";
+            this.Purchaser.ReadOnly = true;
+            this.Purchaser.Width = 150;
+            // 
+            // Purchaseprice
+            // 
+            this.Purchaseprice.HeaderText = "Purchased Price";
+            this.Purchaseprice.MinimumWidth = 8;
+            this.Purchaseprice.Name = "Purchaseprice";
+            this.Purchaseprice.ReadOnly = true;
+            this.Purchaseprice.Width = 150;
+            // 
+            // Saleprice
+            // 
+            this.Saleprice.HeaderText = "Sale Price";
+            this.Saleprice.MinimumWidth = 8;
+            this.Saleprice.Name = "Saleprice";
+            this.Saleprice.ReadOnly = true;
+            this.Saleprice.Width = 150;
+            // 
+            // Profit
+            // 
+            this.Profit.HeaderText = "Profit";
+            this.Profit.MinimumWidth = 8;
+            this.Profit.Name = "Profit";
+            this.Profit.ReadOnly = true;
+            this.Profit.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 470);
+            this.Controls.Add(this.dgvBookSales);
+            this.Controls.Add(this.btnImportfile);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBookSales)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImportfile;
+        private System.Windows.Forms.DataGridView dgvBookSales;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Textbook;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Seller;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Purchaser;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Purchaseprice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Saleprice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Profit;
+    }
+}
+
---
+++ RealSacTask2/RealSacTask2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealSacTask2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImportfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Filedialog = new OpenFileDialog();
+            
+            if (Filedialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = Filedialog.FileName;
+                List<string> lines = new List<string>();
+                lines = File.ReadAllLines(filePath).ToList();
+
+                // Sets the value of the Total Profit
+                float Totalprofit = 0f; ;
+
+                foreach (string line in lines)
+                {
+                    // Splits the data in to seperate columns
+                    List<string> fields = line.Split(',').ToList();
+                    float profit;
+                    //store sale price and purchased price from each line in the file into variables
+                    string salePrice = fields[5];
+                    float purchasedPrice = float.Parse(fields[4]);
+
+                    // Calculate the profit of the textbooks 
+                    if (float.TryParse(salePrice, out float saleprice))
+                    {
+                        profit = saleprice - purchasedPrice;
+                    }
+                    else
+                    {
+                        // The item never got sold, meaning it is a loss and its purchase price is subtracted from the total profit
+                        profit = purchasedPrice * -1;
+                    }
+                    // Add the profit amount to the total profit amount
+                    Totalprofit += profit;
+                    //append the profit field into fields so it is ready to populate the datagridview
+                    fields.Add(profit.ToString());
+
+                    dgvBookSales.Rows.Add(fields.ToArray());
+                }
+
+                //creates an empty row in the datagridview table
+                dgvBookSales.Rows.Add();
+                //counts the length of the rows in the datagridview table
+                int rowCount = dgvBookSales.Rows.Count;
+                //creates and sets the value of the second last cell with text "Total Profit" at the end of the data
+                dgvBookSales.Rows[rowCount - 1].Cells[5].Value = "Total Profit";
+                //creates and sets value of the last cell with the value of the total profit at the end of the data
+                dgvBookSales.Rows[rowCount - 1].Cells[6].Value = $"${Totalprofit.ToString()}";
+
+            }
+        }
+
+    }
+}
